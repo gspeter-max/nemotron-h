8300dc6f1b6ff47642245041c1b6ebf6239d64d4
gspeter-max <gspeter67@gmail.com>
2025-07-04 21:24:44 +0530

    update

--- ppo_trainer.py
+++ ppo_trainer.py
@@ -1,5 +1,6 @@
-from transformers import AutoModelForSequenceClassification , AutoTokenizer , GenerationConfig
+from transformers import AutoModelForSequenceClassification , AutoTokenizer , GenerationConfig , AutoModelForCausalLM
 from trl import PPOConfig , PPOTrainer, AutoModelForCausalLMWithValueHead 
+import trl 
 from datasets import load_dataset 
 import wandb 
 
@@ -12,7 +13,7 @@ wandb.init(
     ''' 
 
 tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased') 
-tokenizer.pad_tokens = tokenizer.eos_token
+tokenizer.pad_token = tokenizer.eos_token
 config = PPOConfig( 
         learning_rate = 0.001,
         do_train = True, 
@@ -24,7 +25,7 @@ config = PPOConfig(
         logging_dir = './ppo_training_config', 
         save_strategy = 'epoch', 
         logging_steps = 10,
-        batch_size = 256, 
+        batch_size = 2, 
         mini_batch_size = 1, 
         gradient_accumulation_steps = 1, 
         ) 
@@ -42,36 +43,39 @@ generation_config = GenerationConfig.from_pretrained(
 )
 
 
-datasets = load_dataset('allura-org/instruct-ppo-mix-20k') 
+datasets = load_dataset('trl-internal-testing/descriptiveness-sentiment-trl-style')
+print(datasets)
+def batch_tokenization(example):
+    data = tokenizer(example['prompt'], truncation= True , padding = 'max_length')
+    return data 
+
+datasets = datasets['descriptiveness'].map(batch_tokenization, batched = True , remove_columns = datasets['descriptiveness'].column_names)
+
 model = AutoModelForCausalLMWithValueHead.from_pretrained('/content/drive/MyDrive/nemotron-h/checkpoint-1636' )
-ref_model = AutoModelForCausalLMWithValueHead.from_pretrained('/content/drive/MyDrive/nemotron-h/checkpoint-1636' )
+ref_model = AutoModelForCausalLM.from_pretrained('/content/drive/MyDrive/nemotron-h/checkpoint-1636' )
 reward_model = AutoModelForSequenceClassification.from_pretrained('/content/drive/MyDrive/reward_model/checkpoint-1125') 
 model.generation_config = generation_config
-print('---------------------------------------------dgfgdf----------------------------------')
+
 ppo_trainer = PPOTrainer(
         args = config, 
         processing_class = tokenizer,
         model = model, 
         ref_model = ref_model, 
-        train_dataset= datasets, 
+        train_dataset = datasets,
         reward_model = reward_model, 
         value_model = reward_model
 
-    ) # it automatically tokenizer that and you able to access this 
-print('--------------------------------------------------------------sdgfs-----------------')
-print(vars(ppo_trainer))
-for epoch in range(ppo_trainer.args.num_train_epochs):
-    for batch in ppo_trainer.dataloader:
-        query = batch['query_batch']
-        # print('------------------------------------------------------------------------')
-        response =  ppo_trainer.generate(query)
-        
-        batch['response'] = tokenizer.decode_batch( response ) 
+    ) # it automatically tokenizer that and you able to access this
+# for epoch in range(ppo_trainer.config.num_train_epochs):
+#     for batch in ppo_trainer.batch:
+#         query = batch['query_batch']
+#         # print('------------------------------------------------------------------------')
+#         response =  ppo_trainer.generate(query)        
+#         batch['response'] = tokenizer.decode_batch( response ) 
 
-        
-        query_response = [ q + a for q , a  in zip( batch['query_batch'] , batch['response'] ) ] 
-        reward_score = reeward_model( query_respone ) 
-        stats = ppo_trainer.step( reward_score , query . response ) 
-        ppo_trainer.log_stats( batch, reward_score , stats ) 
+#         query_response = [ q + a for q , a  in zip( batch['query_batch'] , batch['response'] ) ] 
+#         reward_score = reward_model(query_response) 
+#         stats = ppo_trainer.step( reward_score , query . response ) 
+#         ppo_trainer.log_stats( batch, reward_score , stats ) 
 
-
+ppo_trainer.train()
